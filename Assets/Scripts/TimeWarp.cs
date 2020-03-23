4664c89f47b862b26ad124eb8654c2beece1ab0b
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2020-03-23 13:36:26 +1300

    insomnia scene and main menu

---
+++ Assets/Scripts/TimeWarp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeWarp : MonoBehaviour
+{
+	public float increaseRate = 2.0f;
+
+	private void Update()
+	{
+#if UNITY_EDITOR
+		if (Input.GetKey(KeyCode.T))
+		{
+			Time.timeScale *= (1.0f + Time.unscaledDeltaTime);
+			Debug.Log(Time.timeScale);
+		}
+		else if (Input.GetKeyUp(KeyCode.T))
+		{
+			Time.timeScale = 1.0f;
+		}
+#endif
+
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			QuitButton.Quit();
+		}
+	}
+}
---
+++ Assets/ShowAfterSeconds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowAfterSeconds : MonoBehaviour
+{
+    public float duration = 1.0f;
+    public GameObject visuals;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        visuals.SetActive(false);
+        StartCoroutine(Show());
+    }
+
+    private IEnumerator Show()
+    {
+        yield return new WaitForSeconds(duration);
+        visuals.SetActive(true);
+    }
+}
